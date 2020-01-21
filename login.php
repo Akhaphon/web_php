a3dfed0ed7ad19d360391c461ea0c911510db4ee
akhaphon <605021000787@mail.rmutk.ac.th>
2020-01-21 12:45:21 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBlIC>
+<html>
+        <head>
+        <meta charset="UTF-8">
+        <title>login.php</title>
+        </head>
+        <body>
+          สมาชิก มีดังนี้</br>
+          E-mail : <?php echo $_POST["email"];?></br>
+          password : <?php echo $_POST["password"];?></br></br>
+        
+          <a href = "Home.html">ออกจากระบบ</a>
+          
+        </body>
+</html>
---
+++ register_form.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>CSS Template</title>
+<meta charset="utf-8">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Style the header */
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
+}
+</style>
+<body>
+<header>
+  <h2>EEbook</h2>
+</header>
+    <form name = "form1" method = "post" action="login.php">
+        ชื่อ : 
+        </br><input id="email" name="email" type="text"></br>
+        E-mail : 
+        </br><input id="email" name="email" type="text"></br>
+        password :
+        </br><input id="password" name="password" type="password"></br></br>
+        <input id ="ยืนยัน" name= "Submit" type= "Submit" value = "ยืนยัน">
+            </form>
+</body>
+
+</head>
+</html>
